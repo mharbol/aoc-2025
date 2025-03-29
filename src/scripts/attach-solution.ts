f395f3e17a761670fdfac2436d68ad19eaaf0ebe
Marcus Harbol <mharbol@users.noreply.github.com>
2025-03-28 18:55:23 -0600

    add load default

--- src/scripts/attach-solution.ts
+++ src/scripts/attach-solution.ts
@@ -1,22 +1,18 @@
 
 import { getDaySolution } from '@codegen/importDay'; // generated source file, run `npx astro build` if there are import errors
 
-document.getElementsByName("btn-solve")?.forEach((button) => {
-    let theDay: number = parseInt(button.id);
+const button = document.getElementById("btn-solve")!;
+const dayNumber: number = parseInt(button.getAttribute('name')!);
+var theMod;
+getDaySolution(dayNumber).then(mod => {
+    theMod = mod;
+});
+let part1 = document.getElementById('part-1');
+let part2 = document.getElementById('part-2');
+button.addEventListener("click", () => {
     //@ts-ignore
-    var theMod;
-    getDaySolution(theDay).then(mod => {
-        theMod = mod;
-    });
-    let part1 = document.getElementById('part-1');
-    let part2 = document.getElementById('part-2');
-    button.addEventListener("click", () => {
-        //@ts-ignore
-        const puzzleInput: string = document.getElementById("puzzle-input")?.value;
-        const inputArray: string[] = puzzleInput.split('\n');
-        //@ts-ignore
-        part1.innerHTML = theMod.part1(inputArray);
-        //@ts-ignore
-        part2.innerHTML = theMod.part2(inputArray);
-    });
+    const puzzleInput: string = document.getElementById("puzzle-input")?.value;
+    const inputArray: string[] = puzzleInput.split('\n');
+    part1!.innerHTML = theMod!.part1(inputArray);
+    part2!.innerHTML = theMod!.part2(inputArray);
 });
