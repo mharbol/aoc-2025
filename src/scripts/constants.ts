e83f7e989af3dafee89aa7122065e451220c649b
Marcus Harbol <mharbol@users.noreply.github.com>
2025-04-05 15:19:06 -0600

    font experiment and adding icon

--- src/scripts/constants.ts
+++ src/scripts/constants.ts
@@ -5,7 +5,8 @@ export const YEAR: number = 2025;
 export const DEFAULT_TITLE: string = `Advent of Code ${YEAR}`;
 export const AUTHOR: string = "Marcus Harbol, Software Engineer";
 export const URL_BASE: string = "aoc-2025"
+export const PROJECT_URL: string = `https://github.com/mharbol/${URL_BASE}`;
 
 export function githubUrl(dayNumber: number): string {
-    return `https://github.com/mharbol/${URL_BASE}/blob/master/aoc_2025/src/solution/day${dayNumber.toString().padStart(2, '0')}.rs`
+    return `${PROJECT_URL}/blob/master/aoc_2025/src/solution/day${dayNumber.toString().padStart(2, '0')}.rs`
 }
