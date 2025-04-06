fccca0615ef0abd0cf0c9232510ead254b632afd
Marcus Harbol <mharbol@users.noreply.github.com>
2025-04-05 18:15:33 -0600

    actually have fonts working correctly

--- src/scripts/constants.ts
+++ src/scripts/constants.ts
@@ -3,7 +3,7 @@
 // particularly obvious
 export const YEAR: number = 2025;
 export const DEFAULT_TITLE: string = `Advent of Code ${YEAR}`;
-export const AUTHOR: string = "Marcus Harbol, Software Engineer";
+export const AUTHOR: string = "Marcus Harbol || Software Engineer";
 export const URL_BASE: string = "aoc-2025"
 export const PROJECT_URL: string = `https://github.com/mharbol/${URL_BASE}`;
 
