36d4d06b8f661b34b90393223283c0b1cc08e6f1
Marcus Harbol <mharbol@users.noreply.github.com>
2025-04-08 23:12:49 -0600

    add apple icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,17 +5,26 @@ import { URL_BASE } from './src/scripts/constants';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://mharbol.github.io',
+    site: 'https://mharbol.github.io',
     base: `/${URL_BASE}`,
-  integrations: [
-    srcgen
-  ],
-  markdown: {
-    remarkPlugins: [
-      'remark-gfm', 'remark-smartypants', 'remark-math'
+    vite: {
+        build: {
+            rollupOptions: {
+                external: [
+                    'sharp'
+                ]
+            }
+        }
+    },
+    integrations: [
+        srcgen
     ],
-    rehypePlugins: [
-      'rehype-mathjax'
-    ]
-  }
+    markdown: {
+        remarkPlugins: [
+            'remark-gfm', 'remark-smartypants', 'remark-math'
+        ],
+        rehypePlugins: [
+            'rehype-mathjax'
+        ]
+    }
 });
---
+++ src/pages/[apple].png.ts
@@ -0,0 +1,19 @@
+import sharp from "sharp";
+
+//@ts-ignore
+export async function GET({ params, request }) {
+    const buffer = await sharp("public/favicon.svg")
+        .resize(params.width)
+        .ensureAlpha()
+        .flatten({ background: { r: 0, g: 0, b: 0, alpha: 0 } })
+        .toBuffer();
+    return new Response(buffer, {
+        headers: { "Content-Type": "image/png" }
+    });
+}
+
+export function getStaticPaths() {
+    return [
+        { params: { apple: "apple-icon", width: 180 } },
+    ];
+}
