a90291cad8edba221504a7132d51bf95bec41186
Marcus Harbol <mharbol@users.noreply.github.com>
2025-10-28 18:47:27 -0600

    one line day02

--- aoc_2025/src/solution/day02.rs
+++ aoc_2025/src/solution/day02.rs
@@ -1,15 +1,4 @@
 pub fn part1(lines: &Vec<String>) -> String {
-    solve(lines, is_safe)
-}
-
-pub fn part2(lines: &Vec<String>) -> String {
-    solve(lines, is_drop_safe)
-}
-
-fn solve<T>(lines: &Vec<String>, func: T) -> String
-where
-    T: Fn(&Vec<i32>) -> bool,
-{
     lines
         .iter()
         .map(|line| {
@@ -17,40 +6,50 @@ where
                 .map(|num| num.parse::<i32>().unwrap())
                 .collect::<Vec<i32>>()
         })
-        .filter(func)
+        .filter(|nums| {
+            nums.as_slice()
+                .windows(2)
+                .map(|arr| arr[0] - arr[1])
+                .scan(None, |state: &mut Option<bool>, diff| {
+                    Some(if *state.get_or_insert(diff > 0) {
+                        diff > 0 && diff <= 3
+                    } else {
+                        diff < 0 && diff >= -3
+                    })
+                })
+                .all(|c| c)
+        })
         .count()
         .to_string()
 }
 
-fn is_safe(nums: &Vec<i32>) -> bool {
-    nums.as_slice()
-        .windows(2)
-        .map(|arr| arr[0] - arr[1])
-        .scan(None, |state: &mut Option<bool>, diff| {
-            Some(if *state.get_or_insert(diff > 0) {
-                diff > 0 && diff <= 3
-            } else {
-                diff < 0 && diff >= -3
+pub fn part2(lines: &Vec<String>) -> String {
+    lines
+        .iter()
+        .map(|line| {
+            line.split_whitespace()
+                .map(|num| num.parse::<i32>().unwrap())
+                .collect::<Vec<i32>>()
+        })
+        .filter(|nums| {
+            (0..nums.len()).any(|drop_idx| {
+                nums.iter()
+                    .enumerate()
+                    .filter_map(|(idx, num)| if idx == drop_idx { None } else { Some(*num) })
+                    .collect::<Vec<i32>>()
+                    .as_slice()
+                    .windows(2)
+                    .map(|arr| arr[0] - arr[1])
+                    .scan(None, |state: &mut Option<bool>, diff| {
+                        Some(if *state.get_or_insert(diff > 0) {
+                            diff > 0 && diff <= 3
+                        } else {
+                            diff < 0 && diff >= -3
+                        })
+                    })
+                    .all(|c| c)
             })
         })
-        .all(|c| c)
-}
-
-fn is_drop_safe(nums: &Vec<i32>) -> bool {
-    for drop in 0..nums.len() {
-        if is_drop_safe_idx(nums, drop) {
-            return true;
-        }
-    }
-    false
-}
-
-fn is_drop_safe_idx(nums: &Vec<i32>, drop_idx: usize) -> bool {
-    is_safe(
-        &nums
-            .iter()
-            .enumerate()
-            .filter_map(|(idx, num)| if idx == drop_idx { None } else { Some(*num) })
-            .collect(),
-    )
+        .count()
+        .to_string()
 }
