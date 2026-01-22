7a38ff69b012d67fb79ad4dc5132e1146a330d5a
Marcus Harbol <mharbol@users.noreply.github.com>
2026-01-21 22:55:22 -0700

    Day 09 start

--- aoc_2025/src/solution/day09.rs
+++ aoc_2025/src/solution/day09.rs
@@ -1,9 +1,9 @@
 pub fn part1(lines: &Vec<String>) -> String {
-    let tiles = parse_tiles(lines);
+    let tiles = parse_coordinates(lines);
     (0..tiles.len() - 1)
         .flat_map(|first| (first + 1..tiles.len()).map(move |second| (first, second)))
-        .map(|(first, second)| (tiles[first], tiles[second]))
-        .map(|((r0, c0), (r1, c1))| (r0.abs_diff(r1) + 1) * (c0.abs_diff(c1) + 1))
+        .map(|(first, second)| (&tiles[first], &tiles[second]))
+        .map(|(c1, c2)| c1.area_with(c2))
         .max()
         .unwrap()
         .to_string()
@@ -13,10 +13,26 @@ pub fn part2(_lines: &Vec<String>) -> String {
     "".to_string()
 }
 
-fn parse_tiles(lines: &Vec<String>) -> Vec<(u64, u64)> {
-    lines
-        .iter()
-        .map(|line| line.split_once(",").unwrap())
-        .map(|(left, right)| (left.parse().unwrap(), right.parse().unwrap()))
-        .collect()
+struct Coordinate {
+    row: u64,
+    col: u64,
+}
+
+impl Coordinate {
+    fn new(row: u64, col: u64) -> Coordinate {
+        Coordinate { row: row, col: col }
+    }
+
+    fn from_string(string: &String) -> Coordinate {
+        let (row, col) = string.split_once(",").unwrap();
+        Coordinate::new(row.parse().unwrap(), col.parse().unwrap())
+    }
+
+    fn area_with(&self, other: &Coordinate) -> u64 {
+        (self.row.abs_diff(other.row) + 1) * (self.col.abs_diff(other.col) + 1)
+    }
+}
+
+fn parse_coordinates(lines: &Vec<String>) -> Vec<Coordinate> {
+    lines.iter().map(Coordinate::from_string).collect()
 }
